9de23705cf80f04fc9f1a01b7b13ed1b70c9e4f7
Tran Tham Khang <101232146+ThamKhang@users.noreply.github.com>
2023-12-19 12:56:22 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,9 +13,9 @@ gen64() {
 }
 install_3proxy() {
     echo "installing 3proxy"
-    URL="https://raw.githubusercontent.com/quayvlog/quayvlog/main/3proxy-3proxy-0.8.6.tar.gz"
-    wget -qO- $URL | bsdtar -xvf-
-    cd 3proxy-3proxy-0.8.6
+    URL="https://github.com/3proxy/3proxy/archive/0.9.4.tar.gz"
+    wget -qO- $URL | tar xz
+    cd 3proxy-0.9.4
     make -f Makefile.Linux
     mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
     cp src/3proxy /usr/local/etc/3proxy/bin/
